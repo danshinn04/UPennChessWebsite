4133d248a8e9377f6b750662efad4b75412773ff
Bruce Wang <bruce.wang15@outlook.com>
2024-10-02 23:54:04 -0400

    test

--- upenn/tailwind.config.ts
+++ upenn/tailwind.config.ts
@@ -1,10 +1,10 @@
-import { PluginAPI } from 'tailwindcss/types/config';
-const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
-const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
+import { PluginAPI } from '../upenn/node_modules/tailwindcss/types/config';
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import {default as flattenColorPalette} from "../upenn/node_modules/tailwindcss/lib/util/flattenColorPalette"
 
 module.exports = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: ["./src/*/.{ts,tsx}"],
   darkMode: "class",
   theme: {
     extend: {},
@@ -12,12 +12,12 @@ module.exports = {
   plugins: [
     function addVariablesForColors({ addBase, theme }: PluginAPI) {
       let allColors = flattenColorPalette(theme("colors"));
-      
+
       // Ensure newVars is a string-to-string mapping
       let newVars: Record<string, string> = Object.fromEntries(
-        Object.entries(allColors).map(([key, val]) => [`--${key}`, String(val)])
+          Object.entries(allColors).map(([key, val]) => [--${key}, String(val)])
       );
-      
+
       addBase({
         ":root": newVars,
       });
